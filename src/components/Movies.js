57589d53948184b5cf1e36e494fcea756904a578
JacksonBey <jacksongrowson@gmail.com>
2020-11-05 10:38:35 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie =>
+        <div>
+          <p>Title: {movie.title}</p>
+          <p>RunTime: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+          {movie.genres.map(genre => 
+            <li>{genre}</li>
+            
+            )}
+            </ul>
+          <br/>
+        </div>
+      )}
     </div>
   );
 };
